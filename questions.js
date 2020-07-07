47cfe499e2636a037836f960746c62a028e1df25
Pechanda <pechandav@gmail.com>
2020-07-07 15:30:54 -0700

    creating questions and answers

--- questions.js
+++ questions.js
@@ -5,3 +5,32 @@ const choiceA= document.getElementById("A");
 const choiceB= document.getElementById("B");
 const choiceC= document.getElementById("C");
 const counter= document.getElementById("counter");
+const scoreDiv= document.getElementById("scoreContainer");
+
+// questions
+
+let questions = [
+    {
+        question: "What does HTML stand for?",
+        choiceA: "Wrong",
+        choiceB: "Correct",
+        choiceC: "Wrong",
+        correct: "B"
+
+    }, {
+        question: "What does JS stand for?",
+        choiceA: "Correct",
+        choiceB: "Wrong",
+        choiceC: "Wrong",
+        correct: "A"
+    }, {
+        question: "What does CSS stand for?",
+        choiceA: "Wrong",
+        choiceB: "Correct",
+        choiceC: "Wrong",
+        correct: "A"
+    }
+
+];
+
+
