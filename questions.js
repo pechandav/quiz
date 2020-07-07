7af7b6cab885ebcfd211964bc2c1931837521861
Pechanda <pechandav@gmail.com>
2020-07-07 12:58:31 -0700

    starting code for jasvascript file, creating elements

---
+++ questions.js
@@ -0,0 +1,7 @@
+const start = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const choiceA= document.getElementById("A");
+const choiceB= document.getElementById("B");
+const choiceC= document.getElementById("C");
+const counter= document.getElementById("counter");
