71408aad3b8cdc0529438a5f4155c406776e72a7
Pechanda <pechandav@gmail.com>
2020-07-07 23:53:08 -0700

    adding more questions

--- questions.js
+++ questions.js
@@ -1,75 +1,84 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-	function showQuestions(questions, quizContainer){
-        var output=[];
-        var answers;
-        for(var i=0; i<questions.length; i++){
-            answers=[];
-            for(letter in questions[i].answers){
-                answers.push("<label>" +)
-            }
-        }
-	}
-
-	function showResults(quizContainer, resultsContainer){
-		// code will go here
-	}
-
-	// show the questions
-	showQuestions(questions, quizContainer);
-
-	// when user clicks submit, show results
-	submitButton.onclick = function(){
-		showResults(quizContainer, resultsContainer);
-	}
-}
-
-// questions
-
+// pos is position of where the user in the test or which question they're up to
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+// this is a multidimensional array with 4 inner array elements with 5 elements inside them
 var questions = [
-    {
-        question: "What does HTML stand for?",
-        choiceA: "Heavy Translated Multiple Language",
-        choiceB: "Hypertext Markup Language",
-        choiceC: "How To Make Language",
-        correct: "B"
-
-    }, {
-        question: "What does JS stand for?",
-        choiceA: "Javascript",
-        choiceB: "J-script",
-        choiceC: "Just Styles",
-        correct: "A"
-    }, {
-        question: "What does CSS stand for?",
-        choiceA: "Corrupt Sheet System",
-        choiceB: "Cascading Style Sheets",
-        choiceC: "Cascading Sheets System",
-        correct: "B"
+  {
+      question: "What does HTML stand for?",
+      a: "Heavy Translated Multiple Language",
+      b: "Hypertext MArkup Language",
+      c: "How To Make a Language",
+      answer: "B"
+    },
+  {
+      question: "What doe JS stand for?",
+      a: "J-script",
+      b: "Just Styles",
+      c: "Javascript",
+      answer: "C"
+    },
+  {
+      question: "What does CSS stand for?",
+      a: "Cascading Style Sheet",
+      b: "Corrupt Sheet System",
+      c: "Cascading Sheets System",
+      answer: "A"
+    },
+  {
+      question: "What does var stand for? ",
+      a: "vary",
+      b: "variant",
+      c: "variable",
+      answer: "C"
     }
-
-];
-
-
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-let count = 0;
-const questionTime = 15; // 15s
-let TIMER;
-let score = 0;
-
-
-start.addEventListener("click",startQuiz);
-
-function startQuiz(){
-    start.style.display="none";
-    renderQuestion();
-    quiz.style.display="block";
-    renderCounter();
-    TIMER= setInterval(renderCounter,1000)
+  ];
+// this get function is short for the getElementById function  
+function get(x){
+  return document.getElementById(x);
 }
-
-
-
-
-
+// this function renders a question for display on the page
+function renderQuestion(){
+  test = get("test");
+  if(pos >= questions.length){
+    test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
+    get("test_status").innerHTML = "Test completed";
+    // resets the variable to allow users to restart the test
+    pos = 0;
+    correct = 0;
+    // stops rest of renderQuestion function running when test is completed
+    return false;
+  }
+  get("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+  
+  question = questions[pos].question;
+  chA = questions[pos].a;
+  chB = questions[pos].b;
+  chC = questions[pos].c;
+  // display the question
+  test.innerHTML = "<h3>"+question+"</h3>";
+  // display the answer options
+  // the += appends to the data we started on the line above
+  test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
+  test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
+  test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
+function checkAnswer(){
+  // use getElementsByName because we have an array which it will loop through
+  choices = document.getElementsByName("choices");
+  for(var i=0; i<choices.length; i++){
+    if(choices[i].checked){
+      choice = choices[i].value;
+    }
+  }
+  // checks if answer matches the correct choice
+  if(choice == questions[pos].answer){
+    //each time there is a correct answer this value increases
+    correct++;
+  }
+  // changes position of which character user is on
+  pos++;
+  // then the renderQuestion function runs again to go to next question
+  renderQuestion();
+}
+// Add event listener to call renderQuestion on page load event
+window.addEventListener("load", renderQuestion);
