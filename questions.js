b829fa86c66f4ff4540812ef828b904bd7ce4e09
Pechanda <pechandav@gmail.com>
2020-07-08 00:02:13 -0700

    timer 15 seconds

--- questions.js
+++ questions.js
@@ -30,6 +30,17 @@ var questions = [
       answer: "C"
     }
   ];
+  var count = 15;
+var interval = setInterval(function(){
+  document.getElementById('count').innerHTML=count;
+  count--;
+  if (count === 0){
+    clearInterval(interval);
+    document.getElementById('count').innerHTML='Done';
+    // or...
+    alert("You're out of time!");
+  }
+}, 1000);
  
 function get(x){
   return document.getElementById(x);
